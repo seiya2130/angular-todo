91a81c809dcf814780824af27864ef403add77ff
Seiya_Satoh <satohseiya0312@gmail.com>
2022-04-11 08:52:20 +0900

    [update]コンポーネントに分割

---
+++ src/app/task-list/task-list.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-task-list',
+  templateUrl: './task-list.component.html',
+  styleUrls: ['./task-list.component.css']
+})
+export class TaskListComponent implements OnInit {
+
+  constructor() { }
+
+  tasks = [
+    {title: '牛乳を買う', done: false, deadline: new Date('2021-01-01')},
+    {title: '可燃ゴミを出す', done: true, deadline: new Date('2020-01-02')},
+    {title: '銀行に行く', done: false, deadline: new Date('2020-01-03')},
+  ];
+
+  newTask = {
+    title: '',
+    deadline: new Date(),
+  };
+
+  ngOnInit(): void {
+  }
+
+  addTask(){
+    this.tasks.push({title: this.newTask.title, done: false, deadline: new Date(this.newTask.deadline)});
+    this.newTask = {
+      title: '',
+      deadline: new Date(),
+    };
+  }
+
+  isOverdue(task: any) {
+    return !task.done && task.deadline < (new Date()).setHours(0, 0, 0, 0);
+  }
+
+}
